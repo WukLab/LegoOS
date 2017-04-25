1963ec32b92db8fc94dc4a548a58c3cd5690ca87
Yizhou Shan <shan13@purdue.edu>
2017-04-24 20:56:45 -0400

    Add ELF header file

---
+++ fs/binfmt_elf.c
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2016-2017 Wuklab, Purdue University. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#include <lego/elf.h>
+#include <lego/sched.h>
+#include <lego/kernel.h>
+#include <lego/binfmts.h>
+
+static int load_elf_binary(struct lego_binprm *bprm)
+{
+	return 0;
+}
+
+struct lego_binfmt elf_format = {
+	.load_binary	= load_elf_binary,
+};
