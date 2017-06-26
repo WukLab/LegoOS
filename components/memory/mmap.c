62c4717d1d5e896727544a85366d56ad94d27070
Yizhou Shan <shan13@purdue.edu>
2017-06-26 15:32:17 -0400

    Add components/memory/mmap file

---
+++ components/memory/mmap.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2016-2017 Wuklab, Purdue University. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#include <lego/mm.h>
+#include <lego/slab.h>
+#include <lego/rbtree.h>
+#include <lego/sched.h>
+#include <lego/kernel.h>
+#include <lego/netmacro.h>
+
+
