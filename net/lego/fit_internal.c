f66514622c89f8ffe9656ea596c8cf101526d17e
Yizhou Shan <shan13@purdue.edu>
2017-06-28 05:24:07 -0400

    t

--- net/lego/fit_internal.c
+++ net/lego/fit_internal.c
@@ -195,11 +195,11 @@ struct pingpong_context *client_init_ctx(int size, int rx_depth, int port, struc
 			.send_cq = ctx->send_cq[i],//ctx->cq
 			.recv_cq = ctx->cq[i%NUM_POLLING_THREADS],
 			.cap = {
-				.max_send_wr = rx_depth + 2,
+				.max_send_wr = 1, //rx_depth + 2,
 				//.max_send_wr = 12000,
 				.max_recv_wr = rx_depth,
-				.max_send_sge = 16,
-				.max_recv_sge = 16
+				.max_send_sge = 10,
+				.max_recv_sge = 1
 			},
 			.qp_type = IB_QPT_RC,
 			.sq_sig_type = IB_SIGNAL_REQ_WR
