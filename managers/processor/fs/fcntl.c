63bb8615c6d48574f0e48f8576e684696c6fff07
Yizhou Shan <shan13@purdue.edu>
2017-08-12 00:58:01 -0400

    Add empty fcntl() syscall

---
+++ managers/processor/fs/fcntl.c
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2016-2017 Wuklab, Purdue University. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#include <lego/stat.h>
+#include <lego/slab.h>
+#include <lego/uaccess.h>
+#include <lego/files.h>
+#include <lego/syscalls.h>
+#include <lego/spinlock.h>
+#include <lego/comp_processor.h>
+
+#include "internal.h"
+
+SYSCALL_DEFINE3(fcntl, unsigned int, fd, unsigned int, cmd, unsigned long, arg)
+{	
+	syscall_enter();
+	return -ENOMEM;
+}
