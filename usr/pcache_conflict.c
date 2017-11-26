74f67ce6e262dccd3592af6e4b68a667a6810338
Yizhou Shan <ys@purdue.edu>
2017-11-26 13:35:03 -0500

    [Test-Program] Update pcache conflict test program

--- usr/pcache_conflict.c
+++ usr/pcache_conflict.c
@@ -1,8 +1,14 @@
+/*
+ * Created by Yizhou Shan, Nov 2017
+ * Used to test pcache eviction/flush/fill functionalities.
+ * See detailed flow explanation below.
+ */
+
 #include "includeme.h"
 
 #include <uapi/processor/pcache.h>
 
-struct pcache_stat pstat;
+static struct pcache_stat pstat;
 
 /* Fill page with random number and return checksum */
 static unsigned int page_fill_random(int *ptr)
@@ -24,19 +30,19 @@ static unsigned int page_fill_random(int *ptr)
 }
 
 void *base;
-int nr_lines = 10;
+int nr_lines = 12;
+unsigned int *csums;
 
-/* write to the first cacheline to test eviction */
+/*
+ * write to the first cacheline to test eviction
+ */
 static void *thread_func(void *arg)
 {
 	int *wl;
 
 	wl = (int *)base;
 	while (1) {
-		int i = 0;
-
-		while (i < 100000) i++;
-		*wl = 100;
+		*wl = 0x666666;
 	}
 }
 
@@ -47,10 +53,10 @@ static int test_set_conflict(void)
 	pthread_t tid;
 
 	base = malloc((long)1024*1024*1024*20);
-	BUG_ON(!base);
+	csums = malloc((sizeof(unsigned int)) * nr_lines);
+	BUG_ON(!base || !csums);
 
-	/* Should get this from syscall or /proc file */
-	stride = 0x10000000;
+	stride = pstat.way_stride;
 
 	base = (void *)round_up((unsigned long)base, PAGE_SIZE);
 
@@ -58,12 +64,44 @@ static int test_set_conflict(void)
 	if (ret)
 		die("fail to create new thread");
 
+	/*
+	 * Flow:
+	 *
+	 * a)
+	 * Walk through nr_line pages, and fill them with
+	 * random generated numbers, save the checksum of each
+	 * page to an array.
+	 *
+	 * During the walk through, some early touched pages will
+	 * be flushed back to memory.
+	 *
+	 * In the second step, we again walk through the same set
+	 * of pages, get the csum and compare with the saved ones.
+	 * If anyone of them does not match, it means the underlying
+	 * pcache_flush has corrupted memory.
+	 *
+	 * b)
+	 * There is another thread trying to write to the first page
+	 * all the time, which is used to test the clflush intergity
+	 * feature. During clflush, the page is marked as RO. So the
+	 * other thread should have a pgfault of permission failed.
+	 *
+	 * It should be taken care of by pcache_do_wp_page().
+	 * After that, its mapping will be invalidated by pcache_flush,
+	 * which means eventually it will have a non-present pgfault,
+	 * or a pcache fill.
+	 *
+	 * Both features have been tested and work fine.
+	 * The concern now comes to the performance.
+	 */
+
 	for (i = 0; i < nr_lines; i++) {
 		void *ptr = base + i * stride;
 		unsigned int csum;
 
 		csum = page_fill_random(ptr);
-		printf("(Generate) %d address: (%p) csum: (%#lx)\n",
+		csums[i] = csum;
+		printf("(Generate) %d address: (%p) csum: (%#x)\n",
 			i, ptr, csum);
 	}
 
@@ -72,8 +110,12 @@ static int test_set_conflict(void)
 		unsigned int csum;
 
 		csum = csum_partial(ptr, PAGE_SIZE, 0);
-		printf("(Verify) %d address: (%p) csum: (%#lx)\n",
-			i, ptr, csum);
+		printf("(Verify) %d address: (%p) pcsum: (%#x) ccsum: (%#x)\n",
+			i, ptr, csums[i], csum);
+		if (csum != csums[i]) {
+			printf(" %d Corrupted memory at %p, previous csum: %#x, current csum: %#x\n",
+				i, ptr, csums[i], csum);
+		}
 	}
 	pthread_join(tid, NULL);
 }
@@ -89,8 +131,12 @@ void print_pstat(struct pcache_stat *pstat)
 
 int main(void)
 {
+	setbuf(stdout, NULL);
+
 	srand(time(NULL));
+
 	pcache_stat(&pstat);
 	print_pstat(&pstat);
+
 	test_set_conflict();
 }
