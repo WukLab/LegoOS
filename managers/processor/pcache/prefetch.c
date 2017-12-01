b8b80be14fa935b88b5593dd5fd0e9006a421339
Yizhou Shan <shan13@purdue.edu>
2017-11-30 22:32:48 -0500

    [WiP] Add victim and prefetch files

---
+++ managers/processor/pcache/prefetch.c
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2016-2017 Wuklab, Purdue University. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+/*
+ * Prefetch facilities
+ */
+
+#include <lego/mm.h>
+#include <lego/wait.h>
+#include <lego/slab.h>
+#include <lego/log2.h>
+#include <lego/hash.h>
+#include <lego/kernel.h>
+#include <lego/pgfault.h>
+#include <lego/syscalls.h>
+#include <lego/jiffies.h>
+#include <processor/pcache.h>
+#include <processor/processor.h>
+
