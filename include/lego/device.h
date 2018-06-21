d83e066f19943c4c28872658072377b5d8880bd9
Yizhou Shan <ys@purdue.edu>
2018-06-20 22:23:00 -0700

    Update device.h

--- include/lego/device.h
+++ include/lego/device.h
@@ -1,4 +1,13 @@
 /*
+ * Copyright (c) 2016-2018 Wuklab, Purdue University. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+/*
  * device.h - generic, centralized driver model
  *
  * Copyright (c) 2001-2003 Patrick Mochel <mochel@osdl.org>
@@ -10,42 +19,18 @@
  * See Documentation/driver-model/ for more information.
  */
 
-#if 0
-#ifndef _DEVICE_H_
-#define _DEVICE_H_
-
-//typedef struct pm_message {
-//        int event;
-//} pm_message_t;
+#ifndef _LEGO_DEVICE_H_
+#define _LEGO_DEVICE_H_
 
 struct device;
 
 struct device_driver {
-	//struct bus_type		*bus;
-
-	//enum probe_type probe_type;
-
-	//const struct of_device_id	*of_match_table;
-	//const struct acpi_device_id	*acpi_match_table;
-
 	int (*probe) (struct device *dev);
 	int (*remove) (struct device *dev);
 	void (*shutdown) (struct device *dev);
-	//int (*suspend) (struct device *dev, pm_message_t state);
-	//int (*resume) (struct device *dev);
-	//const struct attribute_group **groups;
-
-	//const struct dev_pm_ops *pm;
-
 };
 
 struct device {
-	u32		id;	/* device instance */
-	u64		*dma_mask;	/* dma mask (if dma'able device) */
-	u64		*coherent_dma_mask;	/* dma mask alloc_coherent mappings */
-	struct device_driver *driver;	/* which driver has allocated this
-					   device */
 }
 
-#endif /* _DEVICE_H_ */
-#endif
+#endif /* _LEGO_DEVICE_H_ */
