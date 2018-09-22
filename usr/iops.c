57d45518cb99fef1468778d3fae3cc34fc2d0bb0
Yizhou Shan <shan13@purdue.edu>
2018-09-21 23:33:23 -0400

    Update iops bench

--- usr/iops.c
+++ usr/iops.c
@@ -1,14 +1,22 @@
 #include "includeme.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-#define NR_THREADS 		12
+#define NR_THREADS 		1
 #define NR_REQ_PER_THREAD	100000ULL
 #define EXCACHE_SIZE		4096ULL
+
+#define	STORAGE_IOPS
 
 static struct pcache_stat pstat;
 static unsigned long pcache_size;
 static size_t buf_size, stride;
+int fd;
 
 double IOPS[NR_THREADS];
+double read_IOPS[NR_THREADS];
+double write_IOPS[NR_THREADS];
 
 void print_pstat(struct pcache_stat *pstat)
 {
@@ -28,7 +36,16 @@ void print_pstat(struct pcache_stat *pstat)
 struct info {
 	void *buf;
 	int i;
+
+	/*
+	 * Use to do random read/write, within a boundary
+	 *
+	 * file_offset_base ------ file_offset_base+stide
+	 */
+	size_t file_offset_base;
 };
+
+char file_buf[NR_THREADS][EXCACHE_SIZE];
 
 /*
  * Each thread get its own buffer to play with.
@@ -44,11 +61,7 @@ static void *thread_func(void *arg)
 	//printf("%s(): buf: %#lx stride: %#lx REQ_SIZE: %#lx\n",
 	//	__func__, buf, stride, NR_REQ_PER_THREAD * EXCACHE_SIZE);
 
-	if (NR_REQ_PER_THREAD * EXCACHE_SIZE > stride) {
-		printf("Adjust parameters\n");
-		exit(-1);
-	}
-
+#ifndef STORAGE_IOPS
 	gettimeofday(&ts, NULL);
 	for (i = 0; i < NR_REQ_PER_THREAD; i++) {
 		int *bar;
@@ -65,6 +78,30 @@ static void *thread_func(void *arg)
 
 	//printf("SEQ[%d] NR_REQS: %lu, total runtime [%ld.%ld (s)] IOPS: %lf\n",
 	//	info->i, NR_REQ_PER_THREAD, result.tv_sec, result.tv_usec/1000, iops);
+
+#else
+	/* Seq Write */
+	gettimeofday(&ts, NULL);
+	for (i = 0; i < NR_REQ_PER_THREAD; i++)
+		write(fd, file_buf[info->i], EXCACHE_SIZE);
+	gettimeofday(&te, NULL);
+	timeval_sub(&result, &te, &ts);
+
+	usec = result.tv_sec * 1000 * 1000 + result.tv_usec;
+	iops =  NR_REQ_PER_THREAD / usec * 1000000;
+	write_IOPS[info->i] = iops;
+
+	/* Seq Read */
+	gettimeofday(&ts, NULL);
+	for (i = 0; i < NR_REQ_PER_THREAD; i++)
+		read(fd, file_buf[info->i], EXCACHE_SIZE);
+	gettimeofday(&te, NULL);
+	timeval_sub(&result, &te, &ts);
+
+	usec = result.tv_sec * 1000 * 1000 + result.tv_usec;
+	iops =  NR_REQ_PER_THREAD / usec * 1000000;
+	read_IOPS[info->i] = iops;
+#endif
 }
 
 int main(void)
@@ -75,7 +112,7 @@ int main(void)
 	void *buf;
 	struct info infos[NR_THREADS];
 
-	double iops;
+	double iops, read_iops, write_iops;
 
 	setbuf(stdout, NULL);
 
@@ -96,16 +133,25 @@ int main(void)
 	stride = buf_size / nr_threads;
 	printf("nr_threads: %d buf_size: %#lx stride: %#lx\n", nr_threads, buf_size, stride);
 
+#ifndef STORAGE_IOPS
 	/*
 	 * Hook up with file
 	 */
 	buf = malloc(buf_size);
 	if (!buf)
 		exit(-1);
+#else
+	fd = open("/mnt/yilun/twitter_rv.net", O_RDWR);
+	if (fd < 0) {
+		perror("Open: ");
+		exit(-1);
+	}
+#endif
 
 	for (i = 0; i < nr_threads; i++) {
 		infos[i].i = i;
 		infos[i].buf = buf + stride * i;
+		infos[i].file_offset_base = stride * i;
 		ret = pthread_create(&tid[i], NULL, thread_func, &infos[i]);
 		if (ret)
 			die("fail to create new thread");
@@ -115,12 +161,13 @@ int main(void)
 		pthread_join(tid[i], NULL);
 	}
 
-	
 	for (i = 0, iops = 0; i < nr_threads; i++) {
 		iops += IOPS[i];
+		write_iops += write_IOPS[i];
+		read_iops += read_IOPS[i];
 	}
-	printf(" %d threads, Total IOPS: %lf avg: %lf\n",
-		nr_threads, iops, iops/nr_threads);
+	printf(" %d threads, Total IOPS: %lf avg: %lf. Read IOPS: %lf, Write IOPS: %lf\n",
+		nr_threads, iops, iops/nr_threads, read_iops, write_iops);
 
 	printf("main() exit\n");
 	return 0;
