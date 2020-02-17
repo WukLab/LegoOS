6bf768d006b062d4e262ff2a63880b5dd669ca0a
Yuxuan <liuyuxuan3713@gmail.com>
2020-02-17 01:09:28 -0800

    Testing p2m messaging

--- managers/memory/handle_state.c
+++ managers/memory/handle_state.c
@@ -5,11 +5,25 @@
 #include <lego/comp_common.h>
 #include <lego/printk.h>
 
+#include <memory/thread_pool.h>
+
 //TODO: handle_p2m_state_dummy_get
+struct p2m_state_reply {
+    /* nb of bytes read, or error */
+    ssize_t		retval;
+};
 
 void handle_p2m_state_dummy_get(struct p2m_state_struct *payload)
 {
     // Print number from payload
     printk("HEYYY! Handling message for state management: %ld", payload->number);
 //    pr_info("handling message: %ld\n", payload->number);
+
+    ssize_t retval = 6666;
+    struct p2m_read_reply *retbuf;
+    retbuf = thpool_buffer_tx(tb);
+    buf = (char *)retbuf;
+    tb_set_tx_size(tb, sizeof(retval));
+
+    retbuf->retval = retval;
 }
