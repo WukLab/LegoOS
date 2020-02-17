f439d46366f95ea4073f8150357240fd0f3f85c5
Yuxuan <liuyuxuan3713@gmail.com>
2020-02-17 00:08:40 -0800

    Testing p2m messaging

--- managers/memory/handle_state.c
+++ managers/memory/handle_state.c
@@ -1,8 +1,10 @@
 //
 // Created by Yuxuan Liu on 2/16/20.
 //
+#include <lego/kernel.h>
 #include <lego/comp_common.h>
 #include <lego/printk.h>
+
 //TODO: handle_p2m_state_dummy_get
 
 void handle_p2m_state_dummy_get(struct p2m_state_struct *payload)
