c05ca4f1695f8033e8da39266eef6a0cab9ea9e4
Tian <tqiu666@gmail.com>
2020-02-18 14:05:53 -0800

    Fixed typo on opcode.h

--- include/lego/rpc/opcode.h
+++ include/lego/rpc/opcode.h
@@ -129,13 +129,13 @@
 #define RET_ESIGSEGV		((__u32)(RET_SIGNAL_BASE+SIGSEGV)) /* Segmentation fault*/
 
 // State Management
-#define P2M_STATE_BASE          ((__u32)0x70000000)
-//#define P2M_STATE_DUMMY_GET		((__u32)__NR_state_dummy_get)
-//#define P2M_STATE_SAVE		    ((__u32)__NR_state_save)
-//#define P2M_STATE_LOAD		    ((__u32)__NR_state_load)
-#define P2M_STATE_DUMMY_GET		(P2M_STATE_BASE + 1)
-#define P2M_STATE_SAVE		    (P2M_STATE_BASE + 2)
-#define P2M_STATE_LOAD		    (P2M_STATE_BASE + 3)
+//#define P2M_STATE_BASE          ((__u32)0x70000000)
+#define P2M_STATE_DUMMY_GET		((__u32)__NR_dummy_get)
+#define P2M_STATE_SAVE		    ((__u32)__NR_state_save)
+#define P2M_STATE_LOAD		    ((__u32)__NR_state_load)
+//#define P2M_STATE_DUMMY_GET		(P2M_STATE_BASE + 1)
+//#define P2M_STATE_SAVE		    (P2M_STATE_BASE + 2)
+//#define P2M_STATE_LOAD		    (P2M_STATE_BASE + 3)
 
 
 
