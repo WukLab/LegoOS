d85b455ca88e5642a5101f40f2861f589962735b
Vansh Singh <vcsingh@ucsd.edu>
2020-02-28 14:02:20 -0500

    added user program

--- usr/test_recho.c
+++ usr/test_recho.c
@@ -5,7 +5,22 @@
 #include <stdio.h>
 #include <linux/unistd.h>
 #include "includeme.h"
+
 int main(void)
 {
 	printf("nid: %d\n", get_local_nid());
+
+	int my_nid = get_local_nid();
+
+	//SENDER
+	if (my_nid == 0) {
+		printf("HI I'm Sender %d", my_nid);
+		recho(1);
+	}
+
+	// RECEIVER
+	else {
+		printf("HI I'm Receiver %d", my_nid);
+	}
+	
 }
